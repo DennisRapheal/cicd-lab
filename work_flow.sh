5218f7ba6e604864da1814d716efc00bf2b81cf0
dennis <lintinwei20030906@gmail.com>
2026-05-14 23:18:29 +0800

    ci: add github actions: fix prettier format issues

--- work_flow.sh
+++ work_flow.sh
@@ -15,16 +15,6 @@ git push origin feature/ci-observe
 brew install act
 docker compose up -d --build
 
-# modify ci.yaml
-# add your steps here, e.g. lint, test, build etc.
-- name: Type check
-    run: npm run typecheck
-
-- name: Run tests
-    run: npm test
-
-- name: Build app
-    run: npm run build
 
 git add .
 git commit -m "ci: add github actions: type check, test and build steps"
@@ -32,7 +22,35 @@ git push origin feature/ci-observe
 
 # npm test error
 # test/app.test.ts
-expect(response.json()).toEqual({ status: 'fail' });
 git add .
 git commit -m "ci: add github actions: npm test error"
+git push origin feature/ci-observe
+
+# step 1. check code format
+npm run format:check
+> my-app@1.0.0 format:check
+> prettier --check .
+
+Checking formatting...
+[warn] .github/workflows/01_hello.yaml
+[warn] .github/workflows/02_run-test.yaml
+[warn] .github/workflows/ci_111550006.yaml
+[warn] .vscode/settings.json
+[warn] docker-compose.yml
+[warn] snippets/01_hello.yaml
+[warn] snippets/02_run-test.yaml
+[warn] Code style issues found in 7 files. Run Prettier to fix.
+
+# step 2. fix code format
+# 縮排問題，使用 prettier 自動修正
+npx prettier --write .github/workflows/01_hello.yaml
+npx prettier --write .github/workflows/02_run-test.yaml
+npx prettier --write .github/workflows/ci_111550006.yaml # "" -> ''
+npx prettier --write .vscode/settings.json
+npx prettier --write docker-compose.yml
+npx prettier --write snippets/01_hello.yaml
+npx prettier --write snippets/02_run-test.yaml
+
+git add .
+git commit -m "ci: add github actions: fix prettier format issues"
 git push origin feature/ci-observe
